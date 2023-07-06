884c219d2c2b79ae900428edf8e798216317e3d0
Bassant Hassan <91430707+Bassant855@users.noreply.github.com>
2023-07-06 19:34:25 +0300

    Update CategoryPage.jsx

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -13,7 +13,7 @@ const CategoryPage = () => {
         
     },[])
     const getAllProducts = () => {
-        fetch('http://localhost:3000/products')
+        fetch('https://fakestoreapi.com/products')
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
